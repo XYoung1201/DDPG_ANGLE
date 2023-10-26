bcb1aa9fedecf81f8abd51f43d74ef675c4a8ac9
XYoung1201 <huangxiaoyang1201@gmail.com>
2023-10-26 15:12:00 +0800

    add function of build c with python on windows

--- envCodeBuild/build_set.py
+++ envCodeBuild/build_set.py
@@ -2,11 +2,13 @@ from setuptools import setup, Extension
 
 module = Extension('PATTACK',
                    sources=['main_q.cpp'],
-                   include_dirs=['/usr/local/include'],
-                   libraries=['m'],
+                  #  include_dirs=['/usr/local/include'],
+                  #  libraries=['m'],
                    library_dirs=['/usr/local/lib'])
 
 setup(name='PATTACK',
       version='1.0',
       description='Python interface for custom C code',
       ext_modules=[module])
+
+# python3.11 build_set.py build_ext --inplace
